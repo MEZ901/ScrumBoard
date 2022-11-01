e810d411a792f91dc78f53ba816fc4cc1e5de64b
issam <issammez44@gmail.com>
2022-11-01 14:59:10 +0100

    Code optimization: put single button instead of 3 in getTasks function

--- scripts.php
+++ scripts.php
@@ -7,7 +7,7 @@
     //ROUTING
     if(isset($_POST['save']))        saveTask();
     if(isset($_POST['update']))      updateTask();
-    if(isset($_GET['id']))      deleteTask();
+    if(isset($_GET['id']))           deleteTask();
 
     function getTasks($st)
     {
@@ -27,11 +27,15 @@
             $date = $row['task_datetime'];
             $description = $row['description'];
 
-            if($status == 'to do' && $st == 1){
+            if($status == 'to do') $icon = '<i id="'.$id.'status" data="'.$row["status_id"].'" class="bi bi-question-square fs-25px"></i>';
+            else if($status == 'in progress') $icon = '<i id="'.$id.'status" data="'.$row["status_id"].'" class="bi bi-hourglass-split fs-25px"></i>';
+            else if($status == 'done') $icon = '<i id="'.$id.'status" data="'.$row["status_id"].'" class="bi bi-check-square fs-25px"></i>';
+
+            if($status == 'to do' && $st == 1 || $status == 'in progress' && $st == 2 || $status == 'done' && $st == 3){
                 echo(
                     '<button class="task border-0 rounded mb-3 p-2 shadow-sm w-100 d-flex">
                     <div class="">
-                        <i id="'.$id.'status" data="'.$row["status_id"].'" class="bi bi-question-square fs-25px"></i>
+                        '.$icon.'
                     </div>
                     <div class="edit-delete-container">
                         <div class="edit-delete d-flex flex-column justify-content-between">
@@ -55,71 +59,17 @@
                     </div>
                     </button>'
                 );
-                echo('<script>document.getElementById("to-do-tasks-count").innerHTML++</script>');
-            } else if($status == 'in progress' && $st == 2){
-                echo(
-                    '<button class="task border-0 rounded mb-3 p-2 shadow-sm w-100 d-flex">
-                    <div class="">
-                        <i id="'.$id.'status" data="'.$row["status_id"].'" class="bi bi-hourglass-split fs-25px"></i>
-                    </div>
-                    <div class="edit-delete-container">
-                        <div class="edit-delete d-flex flex-column justify-content-between">
-                            <i class="bi bi-pencil-square" data-bs-toggle="modal" data-bs-target="#modal-task" onclick="updateButton('.$id.')"></i>
-                            <span class=""></span>
-                            <a href="scripts.php?id='.$id.'"><i id="trash" class="bi bi-trash3 link-dark"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-start ms-2 d-flex w-100 justify-content-between">
-                        <div>
-                            <div id="'.$id.'title" data="'.$title.'" class="fw-bolder fs-5">'.$title.'</div>
-                            <div class="">
-                                <div id="'.$id.'date" data="'.$date.'" class="text-secondary ms-1">#'.$id.' created in'.$date.'</div>
-                                <div id="'.$id.'description" data="'.$description.'" class="task-description ms-1" title="'.$description.'">'.$description.'</div>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-column justify-content-center align-items-end ms-2 w-25">
-                            <span id="'.$id.'priority" data="'.$row["priority_id"].'" class="priority-type bg-hex text-white p-1 mb-2 rounded-3 text-center">'.$priority.'</span>
-                            <span id="'.$id.'type" data="'.$row["type_id"].'" class="priority-type border-2 border border-hex p-1 rounded-3  text-center">'.$type.'</span>
-                        </div>
-                    </div>
-                    </button>'
-                );
-                echo('<script>document.getElementById("in-progress-tasks-count").innerHTML++</script>');
-            } else if($status == 'done' && $st == 3){
-                echo(
-                    '<button class="task border-0 rounded mb-3 p-2 shadow-sm w-100 d-flex">
-                    <div class="">
-                        <i id="'.$id.'status" data="'.$row["status_id"].'" class="bi bi-check-square fs-25px"></i>
-                    </div>
-                    <div class="edit-delete-container">
-                        <div class="edit-delete d-flex flex-column justify-content-between">
-                            <i class="bi bi-pencil-square" data-bs-toggle="modal" data-bs-target="#modal-task" onclick="updateButton('.$id.')"></i>
-                            <span class=""></span>
-                            <a href="scripts.php?id='.$id.'"><i id="trash" class="bi bi-trash3 link-dark"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-start ms-2 d-flex w-100 justify-content-between">
-                        <div>
-                            <div id="'.$id.'title" data="'.$title.'" class="fw-bolder fs-5">'.$title.'</div>
-                            <div class="">
-                                <div id="'.$id.'date" data="'.$date.'" class="text-secondary ms-1">#'.$id.' created in'.$date.'</div>
-                                <div id="'.$id.'description" data="'.$description.'" class="task-description ms-1" title="'.$description.'">'.$description.'</div>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-column justify-content-center align-items-end ms-2 w-25">
-                            <span id="'.$id.'priority" data="'.$row["priority_id"].'" class="priority-type bg-hex text-white p-1 mb-2 rounded-3 text-center">'.$priority.'</span>
-                            <span id="'.$id.'type" data="'.$row["type_id"].'" class="priority-type border-2 border border-hex p-1 rounded-3  text-center">'.$type.'</span>
-                        </div>
-                    </div>
-                    </button>'
-                );
-                echo('<script>document.getElementById("done-tasks-count").innerHTML++</script>');
+
+                if($status == 'to do') echo('<script>document.getElementById("to-do-tasks-count").innerHTML++</script>');
+                else if($status == 'in progress') echo('<script>document.getElementById("in-progress-tasks-count").innerHTML++</script>');
+                else if($status == 'done') echo('<script>document.getElementById("done-tasks-count").innerHTML++</script>');   
             }
+            
         } 
     } 
 
     function saveTask()
-    {
+    {   
         global $conn;
 
         $title = $_POST["task-title"];
